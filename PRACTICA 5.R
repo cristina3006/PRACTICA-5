f193d55377af618e6a3b81c8852d2be392126104
cristina3006 <Cristinamaliamunoz@hotmail.es>
2023-03-12 13:56:16 +0100

    Update PRACTICA 5.R

--- PRACTICA 5.R
+++ PRACTICA 5.R
@@ -49,3 +49,66 @@ install.packages("ggplot2")
 #EJERCICIO 13
 vector3 = c(21, 45, 33, 98, 34, 90, 67, 87, 45, 11, 73, 38, 28, 15, 50, 57, 12, 87, 29, 1)
 #EJERCICIO 14
+coefVariacion_na <- sd(numArtefactos)/mean(numArtefactos)
+ coefVariacion_na
+ #[1] 0.6684602
+ coefVariacion_v3 <- sd(vector3)/mean(vector3)
+ coefVariacion_v3
+ #[1] 0.6359067
+ 
+ coefVariacion_na2 <- IQR(numArtefactos)/mean(numArtefactos)
+ coefVariacion_na2
+ #[1] 0.8781559
+ 
+ coefVariacion_v32 <- IQR(vector3)/mean(vector3)
+  coefVariacion_v32
+ #[1] 0.917481
+  
+  #Función 1
+  #Calculando coeficiente de variación para numArtefactos_int
+  coefVariacion_na <- sd(numArtefactos)/mean(numArtefactos)
+  coefVariacion_na
+
+    #Calculando coeficiente de variación para vector3
+    coefVariacion_v3 <- sd(vector3)/mean(vector3)
+  coefVariacion_v3
+
+    #Función 2
+    #Calculando coeficiente de variación para numArtefactos_int
+    coefVariacion_na2 <- IQR(numArtefactos)/mean(numArtefactos)
+  coefVariacion_na2
+  #Calculando coeficiente de variación para vector3
+  coefVariacion_v32 <- IQR(vector3)/mean(vector3)
+  coefVariacion_v32
+#EJERCICIO 15
+  
+#EJERCICIO 16
+  
+coeficiente_asimetria <- (mean(vector3) - median(vector3)) / sd(vector3)
+coeficiente_asimetria
+#[1] 0.1553776
+
+#Parte 2: 
+  #Distribucioon de Asimetria Positiva 
+vector1 <- c(2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,150)
+coeficiente_asimetria <- (mean(vector1) - median(vector1)) / sd(vector1)
+coeficiente_asimetria
+#[1] 0.2043709
+
+#Distribución con asimetría negativa:
+vector2 <- c(150,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2)
+coeficiente_asimetria <- (mean(vector2) - median(vector2)) / sd(vector2)
+coeficiente_asimetria
+#[1] 0.2043709
+
+#Distribución simétrica:
+ vector4 <- c(2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,20)
+ coeficiente_asimetria <- (mean(vector4) - median(vector4)) / sd(vector4)
+coeficiente_asimetria
+#[1] -0.008568631
+
+#EJERCICIO 17
+
+library(e1071)
+skewness(vector3)
+#[1] 0.3138528
